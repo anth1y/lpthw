705bc4f2832cdf01e13ce88753037ac20910a78d
Anthony Riley II <anth1y@gmail.com>
2014-02-11 21:12:44 -0800

    More about user input.

---
+++ exp10a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+print "How was your day",
+response = raw_input()
+
+print "What would you like to learn?",
+knowledge = raw_input()
+
+print "So tell me %r, and what tickles your fancy %r?" % ( response, knowledge)
